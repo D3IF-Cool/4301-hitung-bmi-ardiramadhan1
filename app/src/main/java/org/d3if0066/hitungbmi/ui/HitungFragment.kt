c018591bcda8fa66e282de3564969612aeeea05a
Muhammad_Ardi <ardi.ramdhan32@gmail.com>
2021-03-16 21:51:35 +0700

    Membuat navigasi ke app lain

--- app/src/main/java/org/d3if0066/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if0066/hitungbmi/ui/HitungFragment.kt
@@ -1,5 +1,6 @@
 package org.d3if0066.hitungbmi.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
@@ -18,10 +19,12 @@ class HitungFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.option_menu, menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_about) {
             findNavController().navigate(
-                R.id.action_hitungFragment_to_aboutFragment)
+                R.id.action_hitungFragment_to_aboutFragment
+            )
             return true
         }
         return super.onOptionsItemSelected(item)
@@ -44,6 +47,7 @@ class HitungFragment : Fragment() {
                 HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
+        binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -73,8 +77,33 @@ class HitungFragment : Fragment() {
 
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
         binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-        binding.saranButton.visibility = View.VISIBLE
+        binding.buttonGroup.visibility = View.VISIBLE
     }
+
+    private fun shareData() {
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val gender = if (selectedId == R.id.priaRadioButton)
+            getString(R.string.pria)
+        else
+            getString(R.string.wanita)
+        val message = getString(
+            R.string.bagikan_template,
+            binding.beratEditText.text,
+            binding.tinggiEditText.text,
+            gender,
+            binding.bmiTextView.text,
+            binding.kategoriTextView.text
+        )
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager
+            ) != null
+        ) {
+            startActivity(shareIntent)
+        }
+    }
+
 
     private fun getKategori(bmi: Float, isMale: Boolean): String {
         kategoriBmi = if (isMale) {
